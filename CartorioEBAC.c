ea97247156a5ee5743cb6107468931ba8cf00b69
Pamelladev <156671847+Pamelladev@users.noreply.github.com>
2024-01-16 09:45:41 +0900

    opcao de saida
    inclusao de saida do sistema do usuario

--- CartorioEBAC.c
+++ CartorioEBAC.c
@@ -134,7 +134,8 @@ int main()
 	    printf("Escolha a opção desejada no menu:\n\n");
     	printf("\t1- Registrar Nomes\n");
     	printf("\t2- Consultar Nomes\n");
-    	printf("\t3- Deletar Nomes\n\n");
+    	printf("\t3- Deletar Nomes\n");
+    	printf("\t4- Sair do Sistema\n\n");
     	printf("opção: ");//fim do menu
     	
 	
@@ -155,6 +156,11 @@ int main()
 	     	case 3:
 			deletar();
 	        break;
+	        
+	        case 4:
+	        printf("Obrigada por utilizar o sistema!\n");
+			return 0;
+			break;	
 	        
 	        default:
 	        printf("Essa opção não está disponível!\n");
